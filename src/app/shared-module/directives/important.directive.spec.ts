ac3918809d6a17c802b23593aaa1cecf0e5280f4
annatrzeciak <ania.trzeciak.3@wp.pl>
2017-12-18 23:28:45 +0100

    Auto-generated commit

--- src/app/shared-module/directives/important.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImportantDirective } from './important.directive';
-
-describe('ImportantDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImportantDirective();
-    expect(directive).toBeTruthy();
-  });
-});
